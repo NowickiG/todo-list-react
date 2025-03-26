d3a4b556dfe28857f8400fbe18cef8829258ce97
Grzregorz Nowicki <agip240571@wp.pl>
2025-03-26 19:46:07 +0100

    sample list removed

--- src/App.js
+++ src/App.js
@@ -11,10 +11,7 @@ import Container from "./Container";
 function App() {
   const [hideDone, setHideDone] = useState(false);
   const [tasks, setTasks] = useState(
-    [
-      { id: 1, content: "przejść na React", done: false },
-      { id: 2, content: "zjeść kolację ", done: true }
-    ]
+    [ ]
   );
 
   const toggleHideDone = () => {
